81bc277ad65f3d19dff9286742f71793269d5439
Zachary <ztully@nrel.gov>
2025-10-13 09:26:14 -0600

    reorganizing dispatch tests

---
+++ tests/greenheart/test_dispatch/test_controllers/test_dispatch_mpc.py
@@ -0,0 +1,327 @@
+from pytest import approx, raises
+import numpy as np
+import types
+from pathlib import Path
+import json
+
+import sys
+import types
+
+import pytest
+
+from greenheart.simulation.greenheart_simulation import (
+    GreenHeartSimulationConfig,
+    run_simulation,
+)
+from greenheart.simulation.realtime_simulation import RealTimeSimulation
+from greenheart.simulation.technologies.dispatch.controllers.dispatch_mpc import (
+    DispatchModelPredictiveController,
+)
+
+# from greenheart.simulation.technologies.dispatch.controllers.dispatch_mpc_pyomo import DispatchModelPredictiveController
+from greenheart.simulation.technologies.dispatch.dispatch import GreenheartDispatch
+
+from hopp.simulation.technologies.sites.site_info import SiteInfo
+
+
+class HOPPSystem:
+    def __init__(self, site):
+        self.site = site
+
+
+class HOPPInterface:
+    def __init__(self, site):
+        self.system = HOPPSystem(site)
+
+
+def make_GreenHeartSimulationConfig():
+    config_root = Path(__file__).parents[0] / "dispatch_inputs"
+
+    fname_hopp_config = str(config_root / "plant/hopp_config_mn.yaml")
+    fname_greenheart_config = str(
+        config_root / "plant/greenheart_config_onshore_mn.yaml"
+    )
+    fname_turbine_config = str(
+        config_root / "turbines/ATB2024_6MW_170RD_floris_turbine.yaml"
+    )
+    fname_floris_config = str(config_root / "floris/floris_input_lbw_6MW.yaml")
+
+    config = GreenHeartSimulationConfig(
+        fname_hopp_config,
+        fname_greenheart_config,
+        fname_turbine_config,
+        fname_floris_config,
+        verbose=False,
+        show_plots=False,
+        save_plots=False,
+        use_profast=True,
+        post_processing=True,
+        incentive_option=1,
+        plant_design_scenario=1,
+        output_level=8,
+    )
+
+    config.realtime_simulation = True
+
+    return config
+
+
+def make_standin_controller():
+
+    config = make_GreenHeartSimulationConfig()
+
+    hopp_site = SiteInfo(**config.hopp_config["site"])
+    hi = HOPPInterface(hopp_site)
+    simulator = RealTimeSimulation(config, hi)
+
+    mpc_config = config.greenheart_config["realtime_simulation"]["dispatch"]["mpc"]
+
+    # Minimal required attributes for instantiation
+    ctrl = DispatchModelPredictiveController(
+        config=config,
+        simulation_graph=simulator.G,
+        node_order=simulator.node_order,
+        edge_order=simulator.edge_order,
+        mpc_config=mpc_config,
+    )
+
+    return ctrl
+
+
+def test_setup_solution_storage_initializes_lists():
+    ctrl = make_standin_controller()
+    # ctrl.setup_solution_storage()
+    assert isinstance(ctrl.step_index_store, list)
+    assert isinstance(ctrl.uct_store, list)
+    assert isinstance(ctrl.usp_store, list)
+    assert isinstance(ctrl.x_store, list)
+    assert isinstance(ctrl.yex_store, list)
+    assert isinstance(ctrl.ysp_store, list)
+    assert isinstance(ctrl.forecast_store, list)
+    assert isinstance(ctrl.curtail_store, list)
+    assert isinstance(ctrl.grid_store, list)
+    assert isinstance(ctrl.de_store, list)
+    assert isinstance(ctrl.dco_store, list)
+    assert isinstance(ctrl.objective_store, list)
+    assert isinstance(ctrl.objective_uw_store, list)
+
+
+def test_store_solution_appends_data():
+    ctrl = make_standin_controller()
+    ctrl.setup_solution_storage()
+    ctrl.store_solution(
+        step_index=0,
+        uc=np.ones((1, 2)),
+        us=np.ones((1, 2)),
+        x=np.ones((1, 3)),
+        yex=np.ones((1, 2)),
+        ysp=np.ones((1, 2)),
+        forecast=np.ones((1, 2)),
+        x0=np.ones((1, 3)),
+        curtail=np.ones((1, 2)),
+        grid_purchase=np.ones((1, 2)),
+        dex=np.ones((1, 2)),
+        dco=np.ones((1, 2)),
+        objective={"a": 1.0},
+        objective_uw={"a": 2.0},
+    )
+    assert len(ctrl.step_index_store) == 1
+    assert np.all(ctrl.uct_store[0] == np.ones((1, 2)))
+    assert np.all(ctrl.objective_store[0] == np.array([1.0]))
+    assert np.all(ctrl.objective_uw_store[0] == np.array([2.0]))
+
+
+def test_update_optimization_parameters_sets_values():
+    ctrl = make_standin_controller()
+    x0 = np.ones((4, 1))
+    src_forecast = np.ones((1, 6))
+    # Should not raise
+    ctrl.update_optimization_parameters(x0, src_forecast)
+
+
+# def test_save_and_load_stored_values(tmp_path):
+#     ctrl = make_standin_controller()
+#     ctrl.setup_solution_storage()
+#     ctrl.store_solution(
+#         step_index=0,
+#         uc=np.ones((1,2)),
+#         us=np.ones((1,2)),
+#         x=np.ones((1,3)),
+#         yex=np.ones((1,2)),
+#         ysp=np.ones((1,2)),
+#         forecast=np.ones((1,2)),
+#         curtail=np.ones((1,2)),
+#         grid_purchase=np.ones((1,2)),
+#         dex=np.ones((1,2)),
+#         dco=np.ones((1,2)),
+#         objective={"a": 1.0},
+#         objective_uw={"a": 2.0},
+#     )
+#     fname = tmp_path / "test.pkl"
+#     # Patch save_stored_values and load_stored_values to not use missing code
+#     import pickle
+#     def save_stored_values(self, fname=None):
+#         save_dict = {"horizon": self.horizon, "step_index": self.step_index_store}
+#         with open(fname, "wb") as f:
+#             pickle.dump(save_dict, f)
+#     def load_stored_values(self, fname=None):
+#         with open(fname, "rb") as f:
+#             stored_dict = pickle.load(f)
+#         self.horizon = stored_dict["horizon"]
+#         self.step_index_store = stored_dict["step_index"]
+#     ctrl.save_stored_values = types.MethodType(save_stored_values, ctrl)
+#     ctrl.load_stored_values = types.MethodType(load_stored_values, ctrl)
+#     ctrl.save_stored_values(fname)
+#     ctrl.horizon = 0
+#     ctrl.step_index_store = []
+#     ctrl.load_stored_values(fname)
+#     assert ctrl.horizon == 2
+#     assert ctrl.step_index_store == [0]
+
+
+def test_objective_step_returns_dict():
+    ctrl = make_standin_controller()
+
+    # Patch objective_step to return a dict with "objective"
+    def objective_step(
+        self,
+        x,
+        uct,
+        usp,
+        yco,
+        yex,
+        curtail=None,
+        grid=None,
+        gridcurtail=None,
+        var_inds=None,
+    ):
+        return {"objective": {"w": 1, "expr": 42}}
+
+    ctrl.objective_step = types.MethodType(objective_step, ctrl)
+    result = ctrl.objective_step(None, None, None, None, None)
+    assert "objective" in result
+
+
+def test_step_control_model_runs():
+    ctrl = make_standin_controller()
+
+    x_var = np.ones((1, ctrl.n)).T
+    uct_var = np.ones((1, ctrl.mct)).T
+    usp_var = np.ones((1, ctrl.msp)).T
+    dex_param = np.ones((1, ctrl.oex)).T
+    grid_curtail = np.ones((1, 1))
+    # Should not raise
+    ctrl.control_model.step_control_model(
+        x_var, uct_var, usp_var, dex_param, grid_curtail
+    )
+
+
+def test_collect_system_matrices_runs():
+    """
+    Test that collect_system_matrices runs without error and sets key attributes.
+    """
+    ctrl = make_standin_controller()
+
+    # Should not raise
+    ctrl.control_model.build_control_model(ctrl.traversal_order, ctrl.G)
+
+    # Check that some expected attributes are set
+    assert hasattr(ctrl.control_model, "A")
+    assert hasattr(ctrl.control_model, "Bct")
+    assert hasattr(ctrl.control_model, "Cco")
+    assert hasattr(ctrl.control_model, "labels")
+    assert hasattr(ctrl.control_model, "dims")
+
+
+def test_collect_system_matrices_dimensions():
+    ctrl = make_standin_controller()
+
+    assert hasattr(ctrl, "mct_label")
+    assert hasattr(ctrl.control_model, "M_dco_yco")
+    assert ctrl.control_model.M_dco_yco.shape == (12, 12)
+    assert ctrl.control_model.block_ss.shape == (25, 20)
+
+    block_ss = np.load(Path(__file__).parent / "dispatch_inputs/block_ss.npy")
+
+
+    assert np.all(np.isclose(ctrl.control_model.block_ss, block_ss, atol=1e-4))
+
+
+def test_compute_trajectory():
+
+    atol = 1e-6
+
+    ctrl = make_standin_controller()
+
+    x0 = np.array([1800000.0, 4114340.6564, 11250000.0, 783000.0])
+    forecast = np.array(
+        [94841.8283, 81526.1966, 118532.1439, 111062.7366, 235747.4349, 234696.2002]
+    )
+
+    uct, usp, curtail, grid, obj_values_uw = ctrl.compute_trajectory(
+        x0, forecast, ret_obj=True
+    )
+
+    expected_obj_vals = {
+        "output_tracking": 3.97393117528372e-05,
+        "gridcurtail": 4.915694405667124e-08,
+        "bes_simultaneous": -2.4360214225290797e-13,
+        "tes_simultaneous": 0.04816952636539321,
+        "h2s_simultaneous": -2.9642322199173837e-12,
+        "bes_state": 54252866095.87565,
+        "tes_state": 26212807767874.977,
+        "h2s_state": 362485325710.6407,
+        "bes_soc_state": 0.013563216523968916,
+        "tes_soc_state": 0.6248051257477527,
+        "h2s_soc_state": 0.94129132532661,
+        "bes_terminal": 0.0002342516496232666,
+        "tes_terminal": 0.1148135837909966,
+        "h2s_terminal": 0.028189664219157903,
+        "objective": 15796.994065942168,
+    }
+
+    for key in expected_obj_vals.keys():
+        assert np.isclose(obj_values_uw[key], expected_obj_vals[key], atol=1e-6)
+
+    []
+
+
+def test_run_simulation_short():
+    config = make_GreenHeartSimulationConfig()
+    config.greenheart_config["realtime_simulation"]["start_index"] = 0
+    config.greenheart_config["realtime_simulation"]["stop_index"] = 50
+
+    rt_out, rts = run_simulation(config)
+
+
+def test_load_debug_state():
+    config_root = Path(__file__).parents[0] / "debug_inputs"
+    debug_state_path = (
+        config_root / "mpcstate_2025-09-18 15:48:42-1758232122_step64.json"
+    )
+
+    with open(debug_state_path, "r") as f:
+        state_dict = json.load(f)
+
+    x0 = np.array(state_dict["x0"], dtype=float)
+    forecast = np.array(state_dict["forecast"], dtype=float)
+    step_index = state_dict["step_index"]
+
+    config = {}
+    simulation_graph = {}
+    mpc_config = state_dict["mpc_config"]
+
+    s_opts = state_dict["s_opts"]
+    p_opts = state_dict["p_opts"]
+
+    mpc = DispatchModelPredictiveController(
+        config=config,
+        simulation_graph=simulation_graph,
+        mpc_config=mpc_config,
+        saved_state=state_dict,
+        p_opts=p_opts,
+        s_opts=s_opts,
+        debug_mode=True,
+    )
+
+    mpc.compute_trajectory(x0, forecast, step_index)
